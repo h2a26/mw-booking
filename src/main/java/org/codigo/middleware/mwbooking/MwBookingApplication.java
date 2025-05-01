8f17dd741686a1353e66c9cb5789647cb151cc35
heinhtetaung <hha.heinms@gmail.com>
2025-05-01 09:47:41 +0630

    Initial commit

---
+++ src/main/java/org/codigo/middleware/mwbooking/MwBookingApplication.java
@@ -0,0 +1,13 @@
+package org.codigo.middleware.mwbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codigo/middleware/mwbooking/MwBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codigo.middleware.mwbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
